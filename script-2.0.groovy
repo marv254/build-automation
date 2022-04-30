5bcaef86464619fee02808eba1d80e2f2cfaa8da
marv254 <60808086+marv254@users.noreply.github.com>
2022-04-30 06:57:25 +0300

    Update script-2.0.groovy

--- script-2.0.groovy
+++ script-2.0.groovy
@@ -5,13 +5,13 @@ def buildJar(){
 
 def buildImage(){
             echo 'building Image of the application'
-            withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-                    sh 'docker build -t 127.0.0.1:8083/java-maven-app:1.0 .'
-                    sh 'echo $PASS | docker login -u $USER --password-stdin 127.0.0.1:8083'
+            withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'USER', passwordVariable: 'PASS')]){
+                    sh 'docker build -t marv254/my-repo:jma-2.6 .'
+                    sh 'echo $PASS | docker login -u $USER --password-stdin'
             } 
 }
 def deployImage(){
-    echo 'deploying image in private nexus repository'
-    sh 'docker push 127.0.0.1:8083/java-maven-app:1.0'
+    echo 'deploying image to Docker Hub'
+    sh 'docker push marv254/my-repo:jma-2.6'
 }
 return this
